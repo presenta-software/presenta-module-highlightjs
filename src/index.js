ae9b9366a20ad21a87169c4a82fc6a892b92381f
abusedmedia <ff@todo.to.it>
2020-10-31 18:06:19 +0100

    dep update and additional option

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import hljs from 'highlight.js'
 
+let injected = false
+
 const injectStyle = style => {
   const protocol = location.protocol.indexOf('file') >= 0 ? 'http:' : ''
   const link = document.createElement('link')
-  link.href = `${protocol}//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.0.0/build/styles/${style}.min.css`
+  link.href = `${protocol}//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.3.2/build/styles/${style}.min.css`
   link.rel = 'stylesheet'
   document.head.appendChild(link)
 }
 
-let injected = false
-
 const module = function (sceneElement, modConfig, sceneConfig, projectConfig) {
-  const style = typeof modConfig === 'string' ? modConfig : 'default'
+  let style = 'default'
+  if (typeof modConfig === 'object') {
+    injected = !!modConfig.noCss
+    style = modConfig.style || 'default'
+  }else {
+    style = typeof modConfig === 'string' ? modConfig : 'default'
+  }
+
+
   if (!injected) {
     injectStyle(style)
     injected = true
